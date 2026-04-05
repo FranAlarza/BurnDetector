7ca2fe1d804b44992d95e87f346476bbd3ecbb65
Fran Alarza <franalarza@gmail.com>
2026-04-05 18:56:04 +0200

    feat(015-sections-into-settings): Add DiskSettingsView placeholder

---
+++ Asado/Features/Disk/Views/DiskSettingsView.swift
@@ -0,0 +1,14 @@
+//
+//  DiskSettingsView.swift
+//  Asado
+//
+//  Created by Fran Alarza
+//
+
+import SwiftUI
+
+struct DiskSettingsView: View {
+    var body: some View {
+        ContentUnavailableView("No settings available", systemImage: "internaldrive")
+    }
+}
